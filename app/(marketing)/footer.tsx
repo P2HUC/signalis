17de91a610745e07ec83dfb2b59ce9e8287e35bf
P2HUC <tonynguyenhz@gmail.com>
2025-09-19 00:51:14 +0700

    4:development

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -6,7 +6,16 @@ export const Footer = () => {
   return (
     <div className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
       <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
-
+          <Button size="lg" variant="ghost" className="w-full cursor-default">
+          <Image
+            src="/hr.svg"
+            alt="Croatian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          ASL
+        </Button>
       </div>
     </div>
   );
